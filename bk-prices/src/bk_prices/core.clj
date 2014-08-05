ebd9a598621181f1cab4485335ab7e4824b940cf
eddiemills <edward.j.mills@gmail.com>
2014-08-06 00:09:23 +0100

    improvements

--- bk-prices/src/bk_prices/core.clj
+++ bk-prices/src/bk_prices/core.clj
@@ -3,6 +3,11 @@
   (:require [net.cgrand.enlive-html :as html])
   (:require [clj-http.client :as client])
 
-  (client/get "http://www.google.co.uk/finance/historical?q=NYSE%3ABKW&ei=JPC6U4CsG8TGwAPR6YHQDA")
+  (defn fetch-url [url]
+    (html/html-resource (java.net.URL. url)))
 
-)
+  (defn url-of [symbol]
+    (fetch-url (str "https://www.google.co.uk/finance/historical?p=" symbol)))
+
+  (defn history-of [symbol]
+    (map html/text (html/select (fetch-url (str "https://www.google.co.uk/finance/historical?q=" symbol)) [:td]))))

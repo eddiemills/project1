8323f1c56ea23ad2f6e09429c19080d6530471f1
eddiemills <edward.j.mills@gmail.com>
2014-08-06 15:56:58 +0100

    moved brackets

--- bk-prices/src/bk_prices/core.clj
+++ bk-prices/src/bk_prices/core.clj
@@ -1,13 +1,13 @@
-(ns bk-prices.core
-  (:gen-class)
-  (:require [net.cgrand.enlive-html :as html])
-  (:require [clj-http.client :as client])
+(ns bk-prices.core)
+(:gen-class)
+(:require [net.cgrand.enlive-html :as html])
+(:require [clj-http.client :as client])
 
-  (defn fetch-url [url]
-    (html/html-resource (java.net.URL. url)))
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
 
-  (defn url-of [symbol]
-    (fetch-url (str "https://www.google.co.uk/finance/historical?q=" symbol)))
+(defn url-of [symbol]
+  (fetch-url (str "https://www.google.co.uk/finance/historical?q=" symbol)))
 
-  (defn history-of [symbol]
-    (map html/text (html/select (url-of symbol) [:td]))))
+(defn history-of [symbol]
+  (map html/text (html/select (url-of symbol) [:td])))
